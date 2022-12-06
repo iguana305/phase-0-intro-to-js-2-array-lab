7f79f61754ddbdfbe214c322cb929e3098418007
Asif Rahman <asif@crandonventures.com>
2022-12-06 01:43:23 -0500

    commit tree 775e99c62af79977fcb18b4f449ce226ec0dca7f

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+} 
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+} 
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+} 
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+} 
+
+const newLastCat = cats.slice();
+function appendCat(name) {
+    newLastCat.push(name);
+    return newLastCat
+} 
+
+const newFirstCat = cats.slice();
+function prependCat(name) {
+    newFirstCat.unshift(name);
+    return newFirstCat
+} 
+
+const lastCatGone = cats.slice();
+function removeLastCat() {
+    lastCatGone.pop();    
+    return lastCatGone
+} 
+
+const firstCatGone = cats.slice();
+function removeFirstCat() {
+    firstCatGone.shift();    
+    return firstCatGone
+} 
